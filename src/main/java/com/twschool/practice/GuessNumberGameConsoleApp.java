e3cbaf673eee1ce7f1999035aa3c97fbb943e3cb
chang <1453749520@qq.com>
2020-06-23 17:35:59 +0800

    finish compute the score for one user in multy-gametimes

--- src/main/java/com/twschool/practice/GuessNumberGameConsoleApp.java
+++ src/main/java/com/twschool/practice/GuessNumberGameConsoleApp.java
@@ -1,101 +1,77 @@
 package com.twschool.practice;
 
-
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import com.twschool.practice.domain.AnswerGenerator;
-import com.twschool.practice.domain.GameAnswer;
 import com.twschool.practice.domain.GameStatus;
 import com.twschool.practice.domain.GuessNumberGame;
-import jdk.nashorn.internal.ir.ContinueNode;
+import org.springframework.util.FastByteArrayOutputStream;
+import org.springframework.util.StringUtils;
 
-import java.text.BreakIterator;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class GuessNumberGameConsoleApp {
+    public static final String SUCCESS_5 = "11111";
+    public static final String SUCCESS_3 = "111";
 
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        boolean controller = true;
+        Scanner scanner = new Scanner(System.in);
 
-    private static List<String> answerNumbers;
-    private static int valueAndPositionCorrectCount;
-    private static int positionCorrectCount;
-    private static int gameTimesInOneMatch;
-    private static List<String> resultOfWinAndLoseForOneUser;
-
-    public GuessNumberGameConsoleApp(String answerString) {
-        GuessNumberGameConsoleApp.answerNumbers = Arrays.asList(answerString.split(" "));
-    }
-
-    public GuessNumberGameConsoleApp() {
-
-    }
-
-    public static void main(String args[]) {
-        gameTimesInOneMatch = 0;
-        String userAnswer = null;
-        GuessNumberGameConsoleApp answer = new GuessNumberGameConsoleApp();
-        AnswerGenerator generateAnswer = new AnswerGenerator();
-        GuessNumberGameConsoleApp.answerNumbers = generateAnswer.generateAnswerForListType();
-        ArrayList<String> userInput = new ArrayList<>();
-        Scanner sc = new Scanner(System.in);
-        while (!GuessNumberGameConsoleApp.answerNumbers.toString().equals(userInput.toString())) {
-            userInput.clear();
-            valueAndPositionCorrectCount = 0;
-            positionCorrectCount = 0;
-            System.out.println("Please guess number:eg,1 2 3 4");
-            for (int i = 0; i < 4; i++) {
-                userInput.add(sc.next());
+        while (controller) {
+            GuessNumberGame guessNumberGame = new GuessNumberGame(new AnswerGenerator());
+            System.out.println("please guess number: eg, 1 2 3 4");
+            while (guessNumberGame.getStatus() == GameStatus.CONTINUED) {
+                String userInput = scanner.nextLine();
+                String result = guessNumberGame.guess(userInput);
+                System.out.println(String.format("guess result: %s", result));
             }
-            if (answer.isRepeat(userInput)) {
-                userInput.clear();
-            } else {
-                userAnswer = getUserInputForString(userInput);
-                String result = answer.check(userAnswer);
-                resultCompareInOneMatch(result);
-                gameTimesInOneMatch++;
+
+            if (guessNumberGame.getStatus() == GameStatus.SUCCEED) {
+                list.add(1);
+                System.out.println("Congratulations, you guess succeed");
+                controller = isContine(controller, scanner);
+            }
+
+            if (guessNumberGame.getStatus() == GameStatus.FAILED) {
+                list.add(0);
+                System.out.println("Sorry, you guess failed");
+                controller = isContine(controller, scanner);
             }
         }
+        calculatePoints(list);
     }
 
-    public static String getUserInputForString(ArrayList<String> userInput) {
-        String userAnswer;
-        userAnswer = userInput.toString().replace("[", "").replace("]", "").replace(",", "");
-        return userAnswer;
+    private static boolean isContine(boolean flag, Scanner scanner) {
+        System.out.println("是否继续游戏：Y or N ");
+        String s = scanner.nextLine();
+        if ("N".equals(s)) {
+            flag = false;
+        }
+        return flag;
     }
 
-    public static void resultCompareInOneMatch(String result) {
-        if (result.equals("4A0B")) {
-            System.out.println(result + "        Congratulations，you win！");
-             //resultOfWinAndLoseForOneUser.add("win");
-            System.exit(0);
-        } else if (gameTimesInOneMatch < 5) {
-            System.out.println(result + "        Sorry,please try again! The left game times：" + (5 - gameTimesInOneMatch));
+    private static void calculatePoints(List<Integer> list) {
+        long score = 0;
+        //计算积分
+        long winNum = list.stream().filter(p -> p == 1).count();
+        long loseNum = list.stream().filter(p -> p == 0).count();
+        score = (winNum - loseNum) * 3;
+
+        String strScore = list.stream().map(r -> r.toString()).collect(Collectors.joining(""));
+
+        int length = strScore.split(SUCCESS_5).length;
+        if (length > 1) {
+            score = score + (length - 1) * 5;
         } else {
-            System.out.println("Sorry，you have failed！The right answer is:" + answerNumbers.toString());
-            //resultOfWinAndLoseForOneUser.add("lose");
-            System.exit(0);
+            length = strScore.split(SUCCESS_3).length;
+            score = score + (length - 1) * 2;
         }
+        System.out.println("您的最终得分为：" + score);
+
     }
-
-    public String check(String userAnswerString) {
-        List<String> userAnswerNumbers = Arrays.asList(userAnswerString.split(" "));
-        valueAndPositionCorrectCount = 0;
-        positionCorrectCount = 0;
-        for (int index = 0; index < answerNumbers.size(); index++) {
-            if (answerNumbers.get(index).equals(userAnswerNumbers.get(index))) {
-                valueAndPositionCorrectCount++;
-            } else if (answerNumbers.contains(userAnswerNumbers.get(index))) {
-                positionCorrectCount++;
-            }
-        }
-        return valueAndPositionCorrectCount + "A" + positionCorrectCount + "B";
-    }
-
-    public boolean isRepeat(List<String> list) {
-        long count = list.stream().distinct().count();
-        if (count < list.size()) {
-            System.out.println("Warning! There exsit some repeat number,please input diffierent number:");
-            return true;
-        }
-        return false;
-    }
-
-
 }
